56732c597c57a726a3278c37e3add0c4f0c5204d
Kleber Carvalho <kleber.carvalho@gmail.com>
2022-06-15 00:05:14 -0300

    updating repo

---
+++ test/unit-test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const message = require('../utils');
+describe('Message Test', () => {
+ it('Welcome Message', () => {
+        assert.strictEqual(message.getWelcomeMessage(), "Welcome to IBM Cloud DevOps using containers. Let\'s go use the Continuous Delivery Service");
+        // assert.strictEqual(message.getWelcomeMessage(), "");
+    });
+ it('Port Test', () => {
+        assert.strictEqual(message.getPortMessage(), "Application Running on port");
+    });
+});
