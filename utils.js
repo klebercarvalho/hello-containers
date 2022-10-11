ebc8612533bc71fb401c34f3d9630c90477f683d
Kleber Carvalho <kleber.carvalho@gmail.com>
2022-10-11 16:24:22 -0300

    IATEC2

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 
 
   
-const welcomeMessage = "Welcome to IBM Cloud DevOps using containers. Let\'s go use the Continuous Delivery Service IATEC";
+const welcomeMessage = "Welcome to IBM Cloud DevOps using containers. Let\'s go use the Continuous Delivery Service IATEC2";
 const portMessage = "Application Running on port";
 
 const getWelcomeMessage = () => {
