abc4c5691d383e7e5fd1ebf9ccb1d8eb7a9fce61
Kleber Carvalho <kleber.carvalho@gmail.com>
2023-10-20 14:13:29 -0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 
 
   
-const welcomeMessage = "Welcome to IBM Cloud DevOps using containers. Let\'s go use the Continuous Delivery Service 202303310027";
+const welcomeMessage = "Welcome to IBM Cloud DevOps using containers. Let\'s go use the Continuous Delivery Service 202310201415";
 const portMessage = "Application Running on port";
 
 const getWelcomeMessage = () => {
